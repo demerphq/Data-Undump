9aa3dc0db787df69c9c2baa0a605e0b3387a5c86
Yves Orton <yves.orton@booking.com>
2012-08-11 12:43:31 +0200

    bump version, add test

--- lib/Data/Undump.pm
+++ lib/Data/Undump.pm
@@ -11,7 +11,7 @@ our @EXPORT_OK = qw( undump );
 our %EXPORT_TAGS = ( 'all' => \@EXPORT_OK );
 our @EXPORT = qw(undump);
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 require XSLoader;
 XSLoader::load('Data::Undump', $VERSION);
